3ebb5f982af5dfe6b1ebb4be540b51e972eacfd2
Tarikkoc <ahmettarikkoc@gmail.com>
2023-07-02 19:29:45 +0300

    end-commit

--- src/components/MediaKit.js
+++ src/components/MediaKit.js
@@ -4,7 +4,7 @@ const MediaKit = () => {
   return (
     <div className="flex flex-col  items-center pt-[100px] pb-[120px] bg-slate-200">
       <div className="flex mediaKit px-[100px] gap-4 justify-center">
-        <div className=" text-TomyaInfo-h2 tomyaInfo-h2">
+        <div className="w-9/12 text-TomyaInfo-h2 tomyaInfo-h2">
           <h2>Basın Kiti</h2>
         </div>
         <div className="text-SocialMedia-h3 socialMedia-h3">
@@ -20,7 +20,7 @@ const MediaKit = () => {
           <span className="text-MediaKit-h6">Logo kullanım kılavuzu</span>
         </div>
         <div className="h-[270px] py-[32px] px-[38px] mediaBox2 flex justfiy-between items-end bg-white">
-          <span className="text-MediaKit-h6 mr-[30px] flex justify-end">
+          <span className="text-MediaKit-h6 mr-[30px]">
             Renkli ve Siyah/Beyaz kullanım
           </span>
           <span className="text-MediaKit-h6 logoBaski">
